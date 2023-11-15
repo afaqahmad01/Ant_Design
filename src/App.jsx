28c3bd7f521cac04f5ac2992e9bdaf9f12221c19
afaq01ahmad <afaq.ahmad@emerald-labs.com>
2023-11-15 16:30:14 +0500

    Temp changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
     <>
       <div>Hello World</div>
       <p>temp is it</p>
-      <div className="bg-red-800 text-white">This shortcuts are working </div>
+      <div className="bg-red-800 text-gray-500">This shortcuts are working</div>
+      <div className="bg-gray-500 text-red-900">This shortcuts are working</div>
     </>
   );
 }
